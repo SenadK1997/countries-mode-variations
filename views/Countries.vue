a54d7522aacf9dd1a39ec8209980e4600f22535a
SenadK1997 <senad.okt97@gmail.com>
2022-09-19 23:57:41 +0200

    pullreq

---
+++ views/Countries.vue
@@ -0,0 +1,25 @@
+<script>
+import NavBar from '../src/components/NavBar.vue';
+export default {
+    name: 'Countries',
+    data() {
+        return {};
+    },
+    components: { NavBar }
+}
+</script>
+<template>
+    <section class="c-section-navbar">
+        <NavBar />
+    </section>
+    <section class="c-section-body">
+
+    </section>
+</template>
+
+<style scoped>
+    .c-section-body {
+        background-color: #fafafa;
+    }
+</style>
+    
